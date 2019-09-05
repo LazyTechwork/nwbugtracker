21095968e80b192a9c2196aeb38cfecef0027971
LazyTechwork <lazytechwork@gmail.com>
2019-09-05 22:59:42 +0300

    Product view changed

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,28 +5,25 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="card w-100">
-                    <h4 class="card-header">{{ $prod->name }} @if($prod->locked)(БЛОКИРОВАН)@endif</h4>
                     <div class="row no-gutters py-2 px-2">
                         <div class="col-md-3">
                             <img src="{{ $prod->getImage() }}" class="card-img-top img-fluid rounded" alt="">
                         </div>
                         <div class="col-md-9">
-                            <div class="card-body pb-0">
-                                <h4>О продукте</h4>
-                                <p class="mb-0">{!! $prod->description !!}</p>
-                            </div>
                             <div class="card-body">
-                                <h4 class="card-title">Статистика</h4>
-                                <h5 class="card-text">Отчётов <span
-                                            class="badge badge-secondary">{{ $prodstat[0] }}</span><br>
-                                    Открыто <span
-                                            class="badge badge-primary">{{ $prodstat[1] }}</span><br>
-                                    В обработке <span
-                                            class="badge badge-warning">{{ $prodstat[2] }}</span><br>
-                                    Исправлено <span
-                                            class="badge badge-success">{{ $prodstat[3] }}</span>
+                                <h4 class="card-title">{{ $prod->name }} @if($prod->locked)(БЛОКИРОВАН)@endif</h4>
+                                <h5>О продукте</h5>
+                                <p>{!! $prod->description !!}</p>
+                                <h5 class="card-text"><span
+                                        class="badge badge-secondary mb-1">Отчётов {{ $prodstat[0] }}</span><br>
+                                    <span
+                                        class="badge badge-primary mb-1">Открыто {{ $prodstat[1] }}</span><br>
+                                    <span
+                                        class="badge badge-warning mb-1">В обработке {{ $prodstat[2] }}</span><br>
+                                    <span
+                                        class="badge badge-success mb-1">Исправлено {{ $prodstat[3] }}</span>
                                 </h5>
-                                <div class="mb-2">
+                                <div class="mb-3">
                                     @if(!$prod->locked)
                                         <a href="{{ route('bugs.newbugV', ['productid'=>$prod->id]) }}" class="btn btn-success">Создать отчёт</a>
                                     @else
@@ -46,12 +43,12 @@
                                         обновление</a>
                                     @endif
                                 </div>
-                                <h4 class="card-title">Обновления</h4>
+                                <h5 class="card-title">Обновления</h5>
                                 @forelse($updates as $vers)
                                     @if($vers->time->lte(\Carbon\Carbon::now()) || $prod->isModerator(session()->get('user_id')) || session()->get('isglmod'))
-                                        <h5>{{ $vers->version }} <sup
+                                        <h6 class="font-weight-bold">{{ $vers->version }} <sup
                                                     class="text-muted">{{ $vers->time->format('d.m.Y H:i') }}</sup>
-                                        </h5>
+                                        </h6>
                                         <p>{!! $vers->changelog !!}</p>
                                         @if(($vers->time->gte(\Carbon\Carbon::now()->addHour()) && $prod->isModerator(session()->get('user_id'))) || session()->get('isglmod'))
                                             <a href="{{ route('products.delupd', ['id'=>$prod->id, 'updateid'=>$vers->id]) }}"
