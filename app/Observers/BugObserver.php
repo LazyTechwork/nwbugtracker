3095fe47bed824908de6af05f5fa91bf1c422412
LazyTechwork <lazytechwork@gmail.com>
2019-09-07 15:34:48 +0300

    New bug notification

---
+++ app/Observers/BugObserver.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Observers;
+
+use App\Bug;
+use ATehnix\VkClient\Client;
+use Illuminate\Support\Facades\DB;
+
+class BugObserver
+{
+    /**
+     * Handle the bug "created" event.
+     *
+     * @param \App\Bug $bug
+     * @return void
+     */
+    public function created(Bug $bug)
+    {
+        $api = new Client('5.101');
+        $api->setDefaultToken('aabb1c8e9ab0e61d5c93f02c11b85b257342c79f522c10b0f148bea0501e6bebdc036a02f7d3f01b33e5b');
+        $chats = DB::table('group_notifications')->get();
+        foreach ($chats as $chat)
+            $api->request('messages.send', [
+                'chat_id' => $chat->id,
+                'message' => sprintf('[%s] %s %s', $bug->getProduct->name, $bug->name, route('bugs.show', ['id' => $bug->id])),
+                'random_id' => $bug->id
+            ]);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Bug;
+use App\Observers\BugObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Bug::observe(BugObserver::class);
     }
 }
